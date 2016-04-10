3d78a5fb618d401bfb9fbd02088f929e1cd7c36f
mckee <mckee@liao.com>
2016-04-11 00:00:25 +0800

    add test

---
+++ log_test.go
@@ -0,0 +1,38 @@
+package glogger
+
+import (
+	"testing"
+	"os"
+)
+
+func TestSetup(t *testing.T) {
+	path:= "/tmp/test_glogger"
+	if err := Setup(path, LevelDebug, DailyRotate);err != nil {
+		t.Errorf("Error at Setup log: %v", err)
+	}
+
+	if Level() != LevelDebug {
+		t.Error("Error at Level()")
+	}
+
+	SetLevel(LevelInfo)
+	if Level() != LevelInfo {
+		t.Error("Error at SetLevel()")
+	}
+
+	Debug("some msg")
+	Debugf("some %s", "msg")
+
+	Info("some msg")
+	Infof("some %s", "msg")
+
+	Warn("some msg")
+	Warnf("some %s", "msg")
+
+	Error("some msg")
+	Errorf("some %s", "msg")
+
+	Close()
+
+	os.RemoveAll(path)
+}
