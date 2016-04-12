7fd08ae119633c96dc4ddf90fee6c9c976a2532c
mckee <mckee@liao.com>
2016-04-12 13:26:32 +0800

    add level_test.go

---
+++ level_test.go
@@ -0,0 +1,15 @@
+package glogger
+
+import (
+	"testing"
+)
+
+func TestString(t *testing.T) {
+	if LevelInfo.String() != "INFO" {
+		t.Errorf("expected INFO, %v", LevelInfo.String())
+	}
+
+	if Level(100).String() != "<NA>" {
+		t.Errorf("expected <NA>, %v", Level(100).String())
+	}
+}
